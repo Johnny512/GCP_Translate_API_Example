f3b810f090f404f806bbeb481249714657e14335
Johnny Castillo <35811688+Johnny512@users.noreply.github.com>
2019-10-03 15:33:44 -0500

    Add files via upload
    initial commit

---
+++ translate.py
@@ -0,0 +1,35 @@
+# Imports the Google Cloud client library
+from google.cloud import translate
+
+#Import json and os
+import json, os
+
+#Create environment variable with creds
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\johnny.castillo\\Downloads\\Translation-386123042010.json"
+
+def translate_json(file,target):
+    # Instantiates a client
+    translate_client = translate.Client()
+    print('Calling Google Translate...')
+
+    # Open and load json file into python dictionary
+    with open(file) as json_file:
+        data = json.load(json_file)
+
+    #Create function to accept data and target language then return translated text.
+    def google_translate(text,target):
+        translated_text= translate_client.translate(
+                text,
+                target_language=target)
+        return translated_text['translatedText']
+
+    #Use dictionary comprehension to loop through text and apply translate function to values
+    new_data = {key:google_translate(value,target) for (key, value) in data.items()}
+    print('Translation into {} successful!'.format(target.upper()))
+
+    #Return translated dictionary to write function
+    return new_data
+
+#Write translated values to file
+with open('es.json','w') as outfile:
+    json.dump(translate_json('en.json','es'),outfile,ensure_ascii=True,indent=1)
